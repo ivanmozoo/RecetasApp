8e365034b06f3d355d9e2827054526ceb7a0b15d
Ivan Mozo <ivanmozo03@gmail.com>
2026-02-17 21:15:42 +0100

    Instalación de bootstrap e integración de los componentes en app.html

--- recetas/src/app/app.ts
+++ recetas/src/app/app.ts
@@ -1,9 +1,12 @@
 import { Component, signal } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { RecetasShow } from "./components/recetas-show/recetas-show";
+import { RecetasNavbar } from "./components/recetas-navbar/recetas-navbar";
+import { RecetasFooter } from "./components/recetas-footer/recetas-footer";
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, RecetasShow, RecetasNavbar, RecetasFooter],
   templateUrl: './app.html',
   styleUrl: './app.css'
 })
