23a437a9fc9956fc349d44fffc061819e909e5d3
Ivan Mozo <ivanmozo03@gmail.com>
2026-02-19 15:58:30 +0100

    Añadido hover a los inputs y creada interfaz de receta

---
+++ recetas/src/app/interfaces/receta.ts
@@ -0,0 +1,13 @@
+export interface Receta {
+    id: string;
+    nombre: string;
+    imagen: string;
+    descripcion: string;
+    ingredientes: string[];
+    pasos: string[];
+    tipo: 'desayuno' | 'comida' 
+    | 'cena' | 'postre' 
+    | 'merienda' | 'bebida' 
+    | 'almuerzo';
+
+}
