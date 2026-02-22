c1b5b371324423e0d01d8d13f25c1c32f2c4a391
ivanmozoo <ivanmozo03@gmail.com>
2026-02-22 02:09:11 +0100

    Creado componente para landing page y añadidas rutas

--- recetas/src/app/app.routes.ts
+++ recetas/src/app/app.routes.ts
@@ -1,3 +1,8 @@
 import { Routes } from '@angular/router';
+import { RecetasShow } from './components/recetas-show/recetas-show';
+import { Landing } from './components/landing/landing';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    { path: '', component: Landing },
+    { path: 'recetas', component: RecetasShow }
+];
