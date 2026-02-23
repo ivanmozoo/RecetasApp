f05533d3092d863ea3d8aa2d43f4da11688cb35f
ivanmozoo <ivanmozo03@gmail.com>
2026-02-23 03:09:58 +0100

    Arreglados los For de la receta detallada, reset del scroll al cambiar de vista

--- recetas/src/app/app.ts
+++ recetas/src/app/app.ts
@@ -1,12 +1,11 @@
 import { Component, signal } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
-import { RecetasShow } from "./components/recetas-show/recetas-show";
 import { RecetasNavbar } from "./components/recetas-navbar/recetas-navbar";
 import { RecetasFooter } from "./components/recetas-footer/recetas-footer";
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, RecetasShow, RecetasNavbar, RecetasFooter],
+  imports: [RouterOutlet, RecetasNavbar, RecetasFooter],
   templateUrl: './app.html',
   styleUrl: './app.css'
 })
