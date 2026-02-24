c3a9f9d4adba1a730fd63026fa10258bfe903377
ivanmozoo <ivanmozo03@gmail.com>
2026-02-24 17:37:32 +0100

    Integrado stepper de angular material para crear recetas, y maquetado de dicha vista

--- recetas/src/app/components/crear-receta/crear-receta.ts
+++ recetas/src/app/components/crear-receta/crear-receta.ts
@@ -1,11 +1,32 @@
-import { Component } from '@angular/core';
-
+import {Component, inject} from '@angular/core';
+import {FormBuilder, Validators, FormsModule, ReactiveFormsModule} from '@angular/forms';
+import {MatInputModule} from '@angular/material/input';
+import {MatFormFieldModule} from '@angular/material/form-field';
+import {MatStepperModule} from '@angular/material/stepper';
+import {MatButtonModule} from '@angular/material/button';
+import { RouterLink } from '@angular/router';
 @Component({
   selector: 'app-crear-receta',
-  imports: [],
+  imports: [
+    MatButtonModule,
+    MatStepperModule,
+    FormsModule,
+    ReactiveFormsModule,
+    MatFormFieldModule,
+    MatInputModule,
+    RouterLink
+  ],
   templateUrl: './crear-receta.html',
   styleUrl: './crear-receta.css',
 })
 export class CrearReceta {
+  private _formBuilder = inject(FormBuilder);
+
+  firstFormGroup = this._formBuilder.group({
+    firstCtrl: ['', Validators.required],
+  });
+  secondFormGroup = this._formBuilder.group({
+    secondCtrl: ['', Validators.required],
+  });
 
 }
