b80aaee36f2449010ac69c09a54fb0773d03d52f
ivanmozoo <ivanmozo03@gmail.com>
2026-02-24 17:07:35 +0100

    Generado componente para crear recetas y navegación a este incluida

--- recetas/src/app/app.routes.ts
+++ recetas/src/app/app.routes.ts
@@ -2,6 +2,7 @@ import { Routes } from '@angular/router';
 import { RecetasShow } from './components/recetas-show/recetas-show';
 import { Landing } from './components/landing/landing';
 import { RecetaDetalle } from './components/receta-detalle/receta-detalle';
+import { CrearReceta } from './components/crear-receta/crear-receta';
 
 export const routes: Routes = [
     { path: '', component: Landing },
@@ -12,5 +13,6 @@ export const routes: Routes = [
         loadComponent: () =>
             import('./components/receta-detalle/receta-detalle')
                 .then(m => m.RecetaDetalle)
-    }
+    },
+    { path: 'crear-receta', component: CrearReceta }
 ];
---
+++ recetas/src/app/components/crear-receta/crear-receta.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-crear-receta',
+  imports: [],
+  templateUrl: './crear-receta.html',
+  styleUrl: './crear-receta.css',
+})
+export class CrearReceta {
+
+}
