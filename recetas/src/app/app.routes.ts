7da5a1eb42686c350e9ac1bb44b8506fc8d6c0f4
Ivan Mozo <ivanmozo03@gmail.com>
2026-02-27 18:51:39 +0100

    Creado componente para editar recetas y añadida la navegación a este

--- recetas/src/app/app.routes.ts
+++ recetas/src/app/app.routes.ts
@@ -1,18 +1,22 @@
 import { Routes } from '@angular/router';
 import { RecetasShow } from './components/recetas-show/recetas-show';
 import { Landing } from './components/landing/landing';
-import { RecetaDetalle } from './components/receta-detalle/receta-detalle';
 import { CrearReceta } from './components/crear-receta/crear-receta';
 
 export const routes: Routes = [
     { path: '', component: Landing },
     { path: 'recetas', component: RecetasShow },
-    { path: 'receta-detalle', component: RecetaDetalle },
     {
         path: 'receta-detalle/:id',
         loadComponent: () =>
             import('./components/receta-detalle/receta-detalle')
                 .then(m => m.RecetaDetalle)
+    },
+    {
+        path: 'editar-receta/:id',
+        loadComponent: () =>
+            import('./components/editar-receta/editar-receta')
+                .then(m => m.EditarReceta)
     },
     { path: 'crear-receta', component: CrearReceta }
 ];
---
+++ recetas/src/app/components/editar-receta/editar-receta.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-editar-receta',
+  imports: [],
+  templateUrl: './editar-receta.html',
+  styleUrl: './editar-receta.css',
+})
+export class EditarReceta {
+
+}
