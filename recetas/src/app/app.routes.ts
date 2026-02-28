e2dfbfbbef6a557e505ebcc671db45f33e0e8c4d
ivanmozoo <ivanmozo03@gmail.com>
2026-02-28 18:03:14 +0100

    Añadido el guard a las rutas para redirigir al login para evitar acceso sin tener la sesión iniciada

--- recetas/src/app/app.routes.ts
+++ recetas/src/app/app.routes.ts
@@ -2,21 +2,35 @@ import { Routes } from '@angular/router';
 import { RecetasShow } from './components/recetas-show/recetas-show';
 import { Landing } from './components/landing/landing';
 import { CrearReceta } from './components/crear-receta/crear-receta';
+import { authGuard } from './guards/auth-guard';
 
 export const routes: Routes = [
     { path: '', component: Landing },
-    { path: 'recetas', component: RecetasShow },
+    { path: 'recetas', component: RecetasShow, canActivate: [authGuard] },
     {
         path: 'receta-detalle/:id',
         loadComponent: () =>
             import('./components/receta-detalle/receta-detalle')
-                .then(m => m.RecetaDetalle)
+                .then(m => m.RecetaDetalle),
+        canActivate: [authGuard]
     },
     {
         path: 'editar-receta/:id',
         loadComponent: () =>
             import('./components/editar-receta/editar-receta')
-                .then(m => m.EditarReceta)
+                .then(m => m.EditarReceta),
+        canActivate: [authGuard]
     },
-    { path: 'crear-receta', component: CrearReceta }
+    { path: 'crear-receta', component: CrearReceta, canActivate: [authGuard] },
+    { 
+        path: 'login',
+        loadComponent: () =>
+            import('./components/login/login').then(m => m.Login)
+    },
+    { 
+        path: 'register',
+        loadComponent: () =>
+            import('./components/register/register').then(m => m.Register)
+    },
+    { path: '**', redirectTo: '' }
 ];
